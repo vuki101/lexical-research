9a8f66793ff6ddd819d8c1ee8af8a1a29dc62ae7
Mateo Vuković <203353391+vuki101@users.noreply.github.com>
2025-04-04 19:16:05 +0200

    Start implementing flushMutations function

--- packages/lexical/src/LexicalMutations.ts
+++ packages/lexical/src/LexicalMutations.ts
@@ -1,6 +1,10 @@
 import { LexicalEditor } from './LexicalEditor';
+import { updateEditorSync } from './LexicalUpdates';
 import { getWindow } from './LexicalUtils';
 
+const TEXT_MUTATION_VARIANCE = 100;
+
+let isProcessingMutations = false;
 let lastTextEntryTimeStamp = 0;
 
 function updateTimeStamp(event: Event) {
@@ -9,12 +13,33 @@ function updateTimeStamp(event: Event) {
 
 function initTextEntryListener(editor: LexicalEditor): void {
   if (lastTextEntryTimeStamp === 0) {
-    // TODO: Why can we not just use window object normally?
     getWindow(editor).addEventListener('textInput', updateTimeStamp, true);
+  }
+}
+
+function flushMutations(
+  editor: LexicalEditor,
+  mutations: Array<MutationRecord>,
+  observer: MutationObserver
+): void {
+  isProcessingMutations = true;
+  const shouldFlushTextMutations =
+    performance.now() - lastTextEntryTimeStamp > TEXT_MUTATION_VARIANCE;
+
+  try {
+    updateEditorSync(editor, () => {
+      // TODO: complete this function
+    });
+  } finally {
+    isProcessingMutations = false;
   }
 }
 
 export function initMutationObserver(editor: LexicalEditor): void {
   initTextEntryListener(editor);
-  // TODO: Continue here
+  editor._observer = new MutationObserver(
+    (mutations: Array<MutationRecord>, observer: MutationObserver) => {
+      flushMutations(editor, mutations, observer);
+    }
+  );
 }
--- packages/lexical/src/LexicalUpdates.ts
+++ packages/lexical/src/LexicalUpdates.ts
@@ -1,7 +1,30 @@
-import { LexicalEditor } from "./LexicalEditor";
+import { EditorUpdateOptions, LexicalEditor } from './LexicalEditor';
 
 let activeEditor: null | LexicalEditor = null;
 
 export function internalGetActiveEditor(): LexicalEditor | null {
   return activeEditor;
+}
+
+function $beginUpdate(
+  editor: LexicalEditor,
+  updateFn: () => void,
+  options?: EditorUpdateOptions
+): void {}
+
+/**
+ * A variant of updateEditor that will not defer if it is nested in an update
+ * to the same editor, much like if it was an editor.dispatchCommand issued
+ * within an update
+ */
+export function updateEditorSync(
+  editor: LexicalEditor,
+  updateFn: () => void,
+  options?: EditorUpdateOptions
+): void {
+  if (activeEditor === editor && options === undefined) {
+    updateFn();
+  } else {
+    $beginUpdate(editor, updateFn, options);
+  }
 }
